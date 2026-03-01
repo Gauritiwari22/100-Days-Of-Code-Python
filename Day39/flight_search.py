193f4cab01dfdf60ddf8a20a78c5c4508974675e
Gauritiwari22 <gauritiwari022@gmail.com>
2026-03-02 00:22:20 +0530

    capstone project completed

--- Day39/flight_search.py
+++ Day39/flight_search.py
@@ -1,5 +1,7 @@
 import requests
 import os
+from datetime import datetime, timedelta
+from flight_data import FlightData
 
 TEQUILA_API_KEY = os.environ["TEQUILA_API_KEY"]
 
@@ -9,12 +11,37 @@ headers = {
 
 class FlightSearch:
 
-    def get_destination_code(self, city_name):
-        endpoint = "https://api.tequila.kiwi.com/locations/query"
+    def get_flight(self, destination_code):
+
+        endpoint = "https://api.tequila.kiwi.com/v2/search"
+
+        tomorrow = datetime.now() + timedelta(days=1)
+        six_months = datetime.now() + timedelta(days=180)
+
         params = {
-            "term": city_name,
-            "location_types": "city"
+            "fly_from": "DEL",  # change to your origin
+            "fly_to": destination_code,
+            "date_from": tomorrow.strftime("%d/%m/%Y"),
+            "date_to": six_months.strftime("%d/%m/%Y"),
+            "curr": "INR",
+            "limit": 1,
+            "sort": "price"
         }
+
         response = requests.get(endpoint, headers=headers, params=params)
         data = response.json()
-        return data["locations"][0]["code"]
+
+        try:
+            flight = data["data"][0]
+        except IndexError:
+            return None
+
+        return FlightData(
+            price=flight["price"],
+            origin_city=flight["route"][0]["cityFrom"],
+            origin_airport=flight["route"][0]["flyFrom"],
+            destination_city=flight["route"][0]["cityTo"],
+            destination_airport=flight["route"][0]["flyTo"],
+            out_date=flight["route"][0]["local_departure"].split("T")[0],
+            return_date=flight["route"][1]["local_departure"].split("T")[0],
+        )
--- Day39/main.py
+++ Day39/main.py
@@ -1,13 +1,24 @@
 from data_manager import DataManager
 from flight_search import FlightSearch
+from notification_manager import NotificationManager
 
 data_manager = DataManager()
 flight_search = FlightSearch()
+notification_manager = NotificationManager()
 
 sheet_data = data_manager.get_destination_data()
 
-for row in sheet_data:
-    if row["iataCode"] == "":
-        row["iataCode"] = flight_search.get_destination_code(row["city"])
+for destination in sheet_data:
 
-data_manager.update_destination_codes(sheet_data)
+    flight = flight_search.get_flight(destination["iataCode"])
+
+    if flight and flight.price < destination["lowestPrice"]:
+
+        message = f"""
+Low price alert!
+Only ₹{flight.price} to fly from {flight.origin_city}-{flight.origin_airport}
+to {flight.destination_city}-{flight.destination_airport}
+from {flight.out_date} to {flight.return_date}.
+"""
+
+        notification_manager.send_sms(message)
