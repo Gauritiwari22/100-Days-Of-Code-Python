2b2f55c2228ae2aff937574d14d416b57b368c7d
Gauritiwari22 <gauritiwari022@gmail.com>
2026-03-02 17:52:06 +0530

    Introdution to HTML

---
+++ Day41/Day40.py
@@ -0,0 +1,30 @@
+# "Day41"
+
+# What is HTML?
+# HTML = HyperText Markup Language
+# It is not a programming language.
+# It is a markup language used to structure web pages.
+# It defines:
+# Headings
+# Paragraphs
+# Links
+# Images
+# Lists
+# Page structure
+
+
+# How the Internet Works (High-Level)
+# When you type a website in a browser:
+# Browser sends HTTP request
+# Server responds with HTML file
+# Browser renders it visually
+# HTML is just text until the browser interprets it.
+
+# Some tags in HTML
+# Heading tag = used to wrte headings (available in hierarchial form)  <h1>,<h2>...
+# paragraph tag = used to write paragraphs (main content of the webpage) <p>...</p>
+# line break and horizontal line = <hr> and <br> (self closing tags) 
+# lists = to organise content in list format (points) <li> <ul> for unordered lists
+# image tag = embed image in the webpage <img>
+# anchor tag = embed links in the webpage <a>
+ 
