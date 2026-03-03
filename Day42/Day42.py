708fdc100b484808c4a3f5125c8cf5e40a693387
Gauritiwari22 <gauritiwari022@gmail.com>
2026-03-03 17:42:46 +0530

    Tables + Forms in HTML

---
+++ Day42/Day42.py
@@ -0,0 +1,47 @@
+# "Day 42"
+
+# HTML TABLES
+# <table> - defines table
+# <tr> - table row
+# <td> - table data (cell)
+# <th> - table headings
+
+# Proper table structure
+
+# <table>
+#   <thead>
+#     <tr>
+#       <th>Year</th>
+#       <th>Work</th>
+#     </tr>
+#   </thead>
+
+#   <tbody>
+#     <tr>
+#       <td>2024</td>
+#       <td>Intern</td>
+#     </tr>
+#   </tbody>
+# </table>
+
+# HTML FORMS
+
+# <form>
+#   <label>Your Name:</label> ---- to make labels
+#   <input type="text">
+# </form>
+
+# <form action="mailto:example@email.com" method="post">
+
+# action
+# Where data goes
+# method
+# GET → data visible in URL
+# POST → data hidden in body
+
+# Name Attribute
+
+# <input type="text" name="yourName">
+# Backend reads name=value.
+
+
