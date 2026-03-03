4b52140348178e401b00799b8f5209b6c93265d0
Gauritiwari22 <gauritiwari022@gmail.com>
2026-03-03 17:51:07 +0530

    Introduction to CSS

---
+++ Day43/Day43.py
@@ -0,0 +1,62 @@
+# "Day 43"
+# What is CSS?
+
+# CSS = Cascading Style Sheets
+# It controls styling (Colors, Fonts, Spacing, Layout, Size, Alignment)
+
+# How to Add CSS (3 Ways)
+# 1. Inline CSS
+# <p style="color: red;">Hello</p>
+
+# 2. Internal CSS
+# <style>
+#   body {
+#     background-color: lightblue;
+#   }
+# </style>
+
+# 3. External CSS
+# <link rel="stylesheet" href="styles.css">
+
+# Inside styles.css:
+
+# body {
+#   background-color: lightblue;
+# }
+
+# Common CSS Properties 
+# Background Color
+# body {
+#   background-color: #EAF6F6;
+# }
+# Text Color
+# h1 {
+#   color: #66BFBF;
+# }
+# Font Size
+# p {
+#   font-size: 20px;
+# }
+# Border
+# img {
+#   border-radius: 50%;
+# }
+
+# Class Selector
+# <p class="intro">Hello</p>
+
+# .intro {
+#   color: red;
+# }
+
+# . means class.
+
+# ID Selector
+# <h1 id="main-heading">Hi</h1>
+# #main-heading {
+#   color: green;
+# }
+
+# # means ID.
+
+# IDs must be unique.
