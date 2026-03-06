4cdc1774326d8eb1e13986342c1ba04e018145ce
Gauritiwari22 <gauritiwari022@gmail.com>
2026-03-07 02:04:44 +0530

    Spotify Playlist Using Web Scraping

---
+++ Day46/Day46.py
@@ -0,0 +1,44 @@
+import requests
+from bs4 import BeautifulSoup
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+date = input("Enter date (YYYY-MM-DD): ")
+
+URL = f"https://www.billboard.com/charts/hot-100/{date}"
+
+response = requests.get(URL)
+soup = BeautifulSoup(response.text, "html.parser")
+song_names_spans = soup.select("li ul li h3")
+song_names = [song.getText().strip() for song in song_names_spans]
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id="YOUR_CLIENT_ID",
+        client_secret="YOUR_CLIENT_SECRET",
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
+song_uris = []
+year = date.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} skipped")
+
+playlist = sp.user_playlist_create(
+    user=user_id,
+    name=f"{date} Billboard 100",
+    public=False
+)
+
+sp.playlist_add_items(
+    playlist_id=playlist["id"],
+    items=song_uris
+)
